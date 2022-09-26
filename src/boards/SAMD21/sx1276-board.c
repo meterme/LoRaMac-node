bb24fdfcf3a5902db5db0badf3cd15c8be65159d
Dana <k6jq@comcast.net>
2022-09-26 14:19:17 -0700

    Correct power adjustment for E19-900M30S module
    Actual measurement shows E19-900M30S has a transmit power 17dB greater than setting in 1276

--- src/boards/SAMD21/sx1276-board.c
+++ src/boards/SAMD21/sx1276-board.c
@@ -244,7 +244,7 @@ SX1276SetRfTxPower( int8_t power )
     // XXX: indicate power level with LED_RX, on > 20dBm
     GpioWrite(&LedRx, (power > 20));
 
-    power -= 8;
+    power -= 17; // Adjust for 17dB gain in E19-900M30S module
     
     paConfig = SX1276Read( REG_PACONFIG );
     paDac = SX1276Read( REG_PADAC );
